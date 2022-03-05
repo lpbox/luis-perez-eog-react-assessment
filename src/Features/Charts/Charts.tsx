1f2b771c5d81ca3893fbac01c1b678cffd013bf3
lpbox <gb.jaramillo20@gmail.com>
2022-03-04 22:27:55 -0500

    show line charts for each unit types

--- src/Features/Charts/Charts.tsx
+++ src/Features/Charts/Charts.tsx
@@ -1,14 +1,19 @@
-import React, { FC, useContext, useEffect } from 'react';
-import { groupBy } from 'lodash';
 import { gql, useLazyQuery } from '@apollo/client';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import { groupBy } from 'lodash';
+import moment from 'moment';
+import React, { FC, useContext, useEffect } from 'react';
 import { toast } from 'react-toastify';
 import {
   Legend,
   Line,
-  LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
 } from 'recharts';
-import moment from 'moment';
+import { makeStyles } from '@material-ui/core/styles';
 import { MetricsContext } from '../../contexts/MetricsContext';
 
 const query = gql`
@@ -52,19 +57,34 @@ type MeasurementDataResponse = {
   getMultipleMeasurements: MeasurementData[];
 };
 
+type MeasurmentChartDataItemValue = {
+  [key: string]: number;
+};
+
+type MeasurmentChartDataItem = MeasurmentChartDataItemValue & {
+  at: number;
+  unit: string;
+};
+
+const useStyles = makeStyles({
+  container: {
+    marginBottom: 24,
+  },
+});
+
 const Charts: FC = () => {
+  const classes = useStyles();
   const startTime = new Date().getTime() - 60 * 60 * 1000;
   const { selectedMetrics } = useContext(MetricsContext);
-  const [getMeasurements, {
-    loading, error, data,
-  }] = useLazyQuery<MeasurementDataResponse>(query);
+  const [getMeasurements, { loading, error, data }] = useLazyQuery<MeasurementDataResponse>(query);
 
   useEffect(() => {
     getMeasurements({
       variables: {
-        input: selectedMetrics.map(
-          metric => ({ metricName: metric, after: startTime }),
-        ),
+        input: selectedMetrics.map((metric) => ({
+          metricName: metric,
+          after: startTime,
+        })),
       },
     });
   }, [selectedMetrics.length]);
@@ -78,70 +98,112 @@ const Charts: FC = () => {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const psiMeasurements = (data?.getMultipleMeasurements || []).filter(
-    measurement => measurement.measurements?.[0]?.unit === 'PSI',
+    (measurement) => measurement.measurements?.[0]?.unit === 'PSI',
   );
 
-  const psiMeasurementsGrouped = Object.entries(groupBy(
-    psiMeasurements.map(
-      group => (group.measurements || []).map(
-        measurement => ({
+  const measurementsWithData = (data?.getMultipleMeasurements || []).filter(
+    measurement => (measurement.measurements?.length || 0) > 0,
+  );
+
+  const chartGroups = groupBy(
+    data?.getMultipleMeasurements || [],
+    (measurement) => measurement.measurements?.[0].unit,
+  );
+
+  const metricsGroupedByUnit = measurementsWithData.reduce(
+    (accum: { [key: string]: string[] }, measurement) => {
+      const unit = measurement.measurements?.[0].unit as string;
+      return {
+        ...accum,
+        [unit]: [...(accum[unit] || []), measurement.metric],
+      };
+    }, {},
+  );
+
+  const chartGroupsFormatted: MeasurmentChartDataItem[][] = Object.entries(
+    chartGroups,
+  ).reduce((accum: MeasurmentChartDataItem[][], [unit, groups]) => {
+    const chartData = groupBy(
+      groups
+        .map((group) => (group.measurements || []).map((measurement) => ({
           metric: measurement.metric,
           at: measurement.at,
           value: measurement.value,
-        }),
-      ),
-    ).reduce(
-      (accum, group) => [...accum, ...group],
-      [],
-    ),
-    'at',
-  )).map(([key, items]) => ({
-    at: parseInt(key, 10),
-    ...items.reduce((accum, item) => ({
-      ...accum,
-      [item.metric]: item.value,
-    }), {}),
-  }));
+        })))
+        .reduce((groupAccum, group) => [...groupAccum, ...group], []),
+      'at',
+    );
+
+    const spreadChartData: MeasurmentChartDataItem[] = Object.entries(
+      chartData,
+    ).map(
+      ([key, items]) => ({
+        at: parseInt(key, 10),
+        ...items.reduce(
+          (chartDataAccum, item) => ({
+            ...chartDataAccum,
+            [item.metric]: item.value,
+            unit,
+          }),
+          {},
+        ),
+      } as MeasurmentChartDataItem),
+    );
+
+    return [...accum, spreadChartData];
+  }, []);
 
   return (
     <>
-      <ResponsiveContainer width="100%" height={400}>
-        <LineChart
-          data={psiMeasurementsGrouped}
-          margin={{
-            top: 20,
-            right: 30,
-            left: 20,
-            bottom: 10,
-          }}
+      {chartGroupsFormatted.map(chartGroup => (
+        <ResponsiveContainer
+          key={chartGroup[0].unit}
+          width="100%"
+          height={400}
+          className={classes.container}
         >
-          <XAxis
-            dataKey="at"
-            tickCount={4}
-            minTickGap={160}
-            tickFormatter={timeOffset => moment(timeOffset).format('hh:mm A')}
-            label={{
-              value: 'Time Offset',
-              position: 'bottom',
-              offset: 0,
+          <LineChart
+            data={chartGroup}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 10,
             }}
-            scale="band"
-          />
-          <YAxis label="PSI" type="number" tickCount={4} />
-          {psiMeasurements.map((psiMeasurement, index) => (
-            <Line
-              type="monotone"
-              dataKey={psiMeasurement.metric}
-              stroke={chartColors[index]}
-              dot={false}
+          >
+            <XAxis
+              dataKey="at"
+              tickCount={4}
+              minTickGap={160}
+              tickFormatter={(timeOffset) => moment(timeOffset).format('hh:mm A')}
+              label={{
+                value: 'Time Offset',
+                position: 'bottom',
+                offset: 0,
+              }}
+              scale="band"
             />
-          ))}
-          <Tooltip
-            labelFormatter={timeOffset => moment(timeOffset).format('hh:mm A')}
-          />
-          <Legend verticalAlign='top' />
-        </LineChart>
-      </ResponsiveContainer>
+            <YAxis
+              label={chartGroup[0].unit.toUpperCase()}
+              type="number"
+              tickCount={4}
+            />
+            {metricsGroupedByUnit[chartGroup[0].unit].map((metric, index) => (
+              <Line
+                key={metric}
+                type="monotone"
+                dataKey={metric}
+                stroke={chartColors[index]}
+                dot={false}
+              />
+            ))}
+            <Tooltip
+              labelFormatter={(timeOffset) => moment(timeOffset).format('hh:mm A')}
+            />
+            <Legend verticalAlign="top" />
+          </LineChart>
+        </ResponsiveContainer>
+      ))}
     </>
   );
 };
